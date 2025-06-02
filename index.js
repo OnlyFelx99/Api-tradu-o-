d17ab6e0b63647c5a42dacb1037e8bbe3688c0c6
Felx999 <53024684+OnlyFelx99@users.noreply.github.com>
2025-06-02 17:34:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors()); // Libera CORS para todas as origens
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
